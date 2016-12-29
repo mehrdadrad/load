199e6650b6657f8656472a0639c171f9630e0284
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-12-28 20:17:56 -0800

    init http load testing

---
+++ load.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"sync"
+)
+
+type Load struct {
+	request             *http.Request
+	requests            int
+	workers             int
+	rateLimit           int
+	disabledCompression bool
+	disabledKeepAlive   bool
+}
+
+func NewTest() (Load, error) {
+	var err error
+	l := Load{}
+	l.request, err = http.NewRequest("GET", "https://google.com", nil)
+	l.request.Header.Add("User-Agent", "load")
+	l.requests = 1000
+	l.workers = 50
+	if err != nil {
+		return l, err
+	}
+
+	return l, nil
+}
+
+func (l *Load) Run() {
+	var wg sync.WaitGroup
+	wg.Add(l.workers)
+	for c := 0; c < l.workers; c++ {
+		go func() {
+			defer wg.Done()
+			l.worker(l.requests / l.workers)
+		}()
+	}
+	wg.Wait()
+}
+
+func (l *Load) worker(n int) {
+	tr := &http.Transport{}
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   time.Duration(2) * time.Second,
+	}
+	for i := 0; i < n; i++ {
+		l.do(client)
+		fmt.Printf("!")
+	}
+}
+
+func (l *Load) do(client *http.Client) {
+	var req = new(http.Request)
+	*req = *l.request
+	resp, _ := client.Do(req)
+	resp.Body.Close()
+}
+
+func main() {
+	l, err := NewTest()
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	l.Run()
+}
